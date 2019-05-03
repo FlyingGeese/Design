8e9f5dabdc2e6fa249611036fcc9ad94bd63f2a5
Michael Law <flyinggeese@gmx.com>
2019-05-03 11:04:39 -0400

    raggedy ass Z menu. Will fix later, but it works

--- rwd/main.js
+++ rwd/main.js
@@ -3,7 +3,6 @@ let menuBtn = document.querySelector('#menuBtn')
 zMenu = () => {
   let menu = document.querySelector('#zMenu')
   menu.classList.remove('hidden')
-  menu.classList.add('onScreen')
 }
 
 menuBtn.addEventListener('click', zMenu)
